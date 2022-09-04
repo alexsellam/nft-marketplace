f50e7e4a1d86699b97d7e780405acd009e3af6f2
User <alex.sellam@gmail.com>
2022-09-04 23:43:35 +0200

    routes + layout components

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
-      <h1 className= "text-3xl font-bold underline">
-        Hello world ! 
+      <h1 className="text-3xl font-bold underline">
+        Hello world !
       </h1>
     </div>
-  )
+  );
 }
 
-export default Home; 
